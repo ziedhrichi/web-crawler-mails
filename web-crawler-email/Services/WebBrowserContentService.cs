1c86d16ea128e4af74f2d36e064509b7c94c81b4
Zied HRICHI <dsi.zied.hrichi@gmail.com>
2026-04-18 23:19:48 +0200

    Implement Email crawler service

---
+++ web-crawler-email/Services/WebBrowserContentService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using web_crawler_email.Interfaces;
+
+public class WebContentBrowserService : IWebBrowser
+{
+    public string GetHtml(string url)
+    {
+        try
+        {
+            // Vérifie que le fichier existe
+            if (!File.Exists(url))
+                return string.Empty;
+
+            // Lecture directe du fichier HTML
+            return File.ReadAllText(url);
+        }
+        catch
+        {
+            return string.Empty;
+        }
+    }
+}
